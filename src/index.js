8426f355c4e3eb41670d429115e3a2ce500e1907
Mustafa Shakir <shakirmustafa58@gmail.com>
2022-06-14 11:52:23 +0530

    remove unneccessary fields in amplify config

--- src/index.js
+++ src/index.js
@@ -8,19 +8,12 @@ import Amplify, { Auth } from 'aws-amplify';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 Amplify.configure({
   Auth: {
-    region: 'ap-south-1',
+    region: process.env.REACT_APP_REGION,
     userPoolId: process.env.REACT_APP_USERPOOLID,
     userPoolWebClientId: process.env.REACT_APP_CLIENTID,
     mandatorySignIn: true,
-    oauth: {
-      domain: 'studentportal.auth.ap-south-1.amazoncognito.com',
-      scope: ['email', 'openid', 'phone', 'aws.cognito.signin.user.admin'],
-      redirectSignIn: process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PRODUCTION_ENDPOINT : 'http://localhost:3000',
-      redirectSignOut: process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PRODUCTION_ENDPOINT : 'http://localhost:3000',
-      responseType: 'code',
-    },
     cookieStorage: {
-      domain: process.env.NODE_ENV === 'production' ? "studentportal.shakirmustafa.com" : "localhost",
+      domain: process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PRODUCTION_ENDPOINT : "localhost",
       path: "/",
       expires: 365,
       secure: true
@@ -31,7 +24,7 @@ Amplify.configure({
       {
         name: "student-portal-api",
         endpoint: process.env.REACT_APP_API_ENDPOINT,
-        region: 'ap-south-1',
+        region: process.env.REACT_APP_REGION,
         custom_header: async () => {
           return { Authorization: `Bearer ${(await Auth.currentSession()).getAccessToken().getJwtToken()}` }
         }
